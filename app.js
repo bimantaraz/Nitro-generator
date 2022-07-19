474e5144a9ea2220c2348cdd327ed9d66b86ab5d
Tenclea <tenclea@protonmail.com>
2022-07-19 19:42:09 +0200

    Fixed ratelimit issue

--- app.js
+++ app.js
@@ -122,7 +122,7 @@ process.on('exit', () => { logger.info('Closing YANG... If you liked this projec
 
 					stats.working++;
 				}
-				else if (body.message === 'The resource is being rate limited.') {
+				else if (res.statusCode == 429) {
 					// timeouts equal to 600000 are frozen. Most likely a ban from Discord's side.
 					const timeout = body.retry_after;
 					if (timeout != 600000) {
