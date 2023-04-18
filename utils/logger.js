4b6040910f9a5987f9325c08f85038a4b6f7634d
Tenclea <tenclea@protonmail.com>
2023-04-18 17:04:40 +0200

    Write default config when the file is not found

--- utils/logger.js
+++ utils/logger.js
@@ -1,17 +1,18 @@
 const
 	chalk = require('chalk'),
-	{ readFileSync } = require('fs'),
+	{ existsSync, readFileSync } = require('fs'),
+	{ createDefaultConfigFile } = require('./functions.js'),
 	winston = require('winston'),
 	yaml = require('js-yaml');
 
+if (!existsSync('./config.yml')) createDefaultConfigFile();
 const debug_mode = yaml.load(readFileSync('./config.yml')).debug_mode;
 
 module.exports = winston.createLogger({
 	transports: [new winston.transports.Console()],
 	format: winston.format.printf(log => {
 		const date = new Date();
-		let times = [date.getHours(), date.getMinutes(), date.getSeconds()];
-		times = times.map(t => { if (t < 10) { t = '0' + t; } return t; });
+		const times = [date.getHours(), date.getMinutes(), date.getSeconds()].map(t => t >= 10 ? t : '0' + t);
 
 		const time = chalk.magenta(times.join(':')) + ' ';
 		const message = ` » ${log.message}`;
