7ae66286773652f4b22b1b7c7afbff50eb5f7c8a
Tenclea <tenclea@protonmail.com>
2023-08-27 22:21:49 +0200

    Fix crash when no config file found

--- utils/logger.js
+++ utils/logger.js
@@ -22,31 +22,31 @@ const logger = winston.createLogger({
 	level: debug_mode ? 'debug' : 'info',
 });
 
-const createDefaultConfigFile = () => {
+function createDefaultConfigFile() {
 	const data = `# The length of the codes to generate, can be one of 'short', 'long' or 'both'
 # Short is 16 characters, long is 24, and both will randomly choose between 16 and 24 each time
 code_length: short
 
 # Auto redeems valid nitro codes
 auto_redeem:
-# enable auto_redeem or not (true / false)
-enabled: false
-# The token to redeem the codes with
-token: "PUT_YOUR_TOKEN_HERE"
+  # enable auto_redeem or not (true / false)
+  enabled: false
+  # The token to redeem the codes with
+  token: "PUT_YOUR_TOKEN_HERE"
 
 # Proxy-related settings
 proxies:
-# Write working proxies to a file (true / false)
-save_working: true
+  # Write working proxies to a file (true / false)
+  save_working: true
 
-# Validate proxies before passing them to the checker (true / false)
-enable_checker: true
+  # Validate proxies before passing them to the checker (true / false)
+  enable_checker: true
 
-# Download fresh proxies from the web (true / false)
-enable_scrapper: true
+  # Download fresh proxies from the web (true / false)
+  enable_scrapper: true
 
-# The maximum amount of proxies to download (if enabled), use '0' for no maximum.
-max_proxies_download: 1000
+  # The maximum amount of proxies to download (if enabled), use '0' for no maximum.
+  max_proxies_download: 1000
 
 # The amount of codes to check at the same time
 # The higher, the faster, but it may slow the generator down if it is set too high
@@ -54,22 +54,22 @@ threads: 50
 
 # Receive webhook messages on start and when a valid code is found
 webhook:
-# enable webhook messages or not (true / false)
-enabled: false
+  # enable webhook messages or not (true / false)
+  enabled: false
 
-# The webhook url
-url: https://discord.com/api/webhooks/.../...
+  # The webhook url
+  url: https://discord.com/api/webhooks/.../...
 
-# Webhook notifications settings
-notifications:
-# Send a notification when the generator starts (true / false)
-boot: true
+  # Webhook notifications settings
+  notifications:
+  # Send a notification when the generator starts (true / false)
+  boot: true
 
-# Send a notification when a valid code is found (true / false)
-valid_code: true
+  # Send a notification when a valid code is found (true / false)
+  valid_code: true
 
-# How often to send status updates to the webhook in seconds (use '0' for never)
-status_update_interval: 600
+  # How often to send status updates to the webhook in seconds (use '0' for never)
+  status_update_interval: 600
 
 # Print additional information in the console (true / false)
 debug_mode: false`;
